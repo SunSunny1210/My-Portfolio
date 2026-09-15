7260cd0847e01e82ba92435a4dc6805ecadf3788
Sun Sunny <sol16821@gmail.com>
2026-09-15 19:19:14 +0200

    Add MainTitle and MainView to App.vue.

--- My-Portfolio/src/App.vue
+++ My-Portfolio/src/App.vue
@@ -1,5 +1,19 @@
 <script setup>
+import MainTitle from './components/MainTitle.vue';
+import MainView from './components/views/MainView.vue';
+
 </script>
 
 <template>
+  <div class="body">
+    <MainTitle/>
+    <MainView/>
+  </div>
 </template>
+
+<style scoped>
+.body {
+  height: 100%;
+  width: 100%;
+}
+</style>
