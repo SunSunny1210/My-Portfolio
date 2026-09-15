5d19babefae3b8063cfe8a8cce50157e9e550b3e
Sun Sunny <sol16821@gmail.com>
2026-09-15 18:45:51 +0200

    Create MainView to have better control of Options-Views interaction.

---
+++ My-Portfolio/src/components/views/MainView.vue
@@ -0,0 +1,37 @@
+<script setup>
+import Options from '../Options.vue';
+</script>
+
+<template>
+    <div class="main-view">
+        <Options/>
+        <RouterView v-slot="{ Component }">
+            <Transition name="" mode="out-in">
+                <component :is="Component"/>
+            </Transition>
+        </RouterView>
+    </div>
+</template>
+
+<style scoped>
+.main-view {
+    height: 100%;
+    width: 100%;
+    display: flex;
+    justify-content: flex-end;
+    align-self: center;
+}
+
+.slide-in-enter-active,
+.slide-in-leave-active {
+  transition: all 0.4s ease-in-out;
+}
+
+.slide-in-enter-from {
+  transform: translateX(70%);
+}
+
+.slide-in-leave-to {
+  transform: translateX(100%);
+}
+</style>
